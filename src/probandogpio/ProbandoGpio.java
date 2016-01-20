2dbf9118d82e8f260536e07e6a7dbfab767a4cf8
Enrique Medina <medina9304@gmail.com>
2016-01-20 15:01:12 -0600

    Initial commit

---
+++ src/probandogpio/ProbandoGpio.java
@@ -0,0 +1,54 @@
+
+package probandogpio;
+
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+
+public class ProbandoGpio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)throws InterruptedException {
+        System.out.println("<--Pi4J--> GPIO Listen Example ... started.");
+        
+        // create gpio controller
+        final GpioController gpio = GpioFactory.getInstance();
+
+        // provision gpio pin #02 as an input pin with its internal pull down resistor enabled
+        final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
+
+        // create and register gpio pin listener
+        myButton.addListener(new GpioPinListenerDigital() {
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
+                System.out.println(myButton.getState());
+            }
+            
+        });
+        
+        System.out.println(" ... complete the GPIO #02 circuit and see the listener feedback here in the console.");
+        
+        // keep program running until user aborts (CTRL-C)
+        for (;;) {
+            Thread.sleep(500);
+        }
+        
+        // stop all GPIO activity/threads by shutting down the GPIO controller
+        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
+        // gpio.shutdown();   <--- implement this method call if you wish to terminate the Pi4J GPIO controller        
+    }
+
+
+// END SNIPPET: listen-gpio-snippet
+
+    
+    
+}
